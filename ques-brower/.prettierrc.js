a07c9992966c1e65716168a8b7b1f1ae254a452b
wyy-g <1482581329@qq.com>
2023-12-16 22:40:49 +0800

    前端初始化

---
+++ ques-brower/.prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // 箭头函数只有一个参数的时候可以省略括号
+    arrowParens: 'avoid',
+    // 括号内部不要出现空格
+    backetSpacing: true,
+    // 行结束符使用unix格式
+    endOfLine: 'lf',
+    // 行宽
+    printWidth: 100,
+    // 换行方式
+    proseWrap: 'preserve',
+    // 分号
+    semi: false,
+    // 使用单引号
+    singleQuote: true,
+    // 缩进
+    tabWidth: 2,
+    // 使用tab进行缩进
+    useTabs: true,
+    // 后置逗号，多行对象、数组在最后一行增加逗号
+    // trailingComma: 'es5',
+    parser: 'typescript'
+}
