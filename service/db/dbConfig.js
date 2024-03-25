aadb7ea6fda50d1019147edeef596ba93a06b15c
wyy-g <1482581329@qq.com>
2024-03-25 22:11:26 +0800

    BugFix: 提交反馈不需要登录

--- service/db/dbConfig.js
+++ service/db/dbConfig.js
@@ -1,7 +1,7 @@
 const DB_HOST = 'localhost'
 const DB_USER = 'root'
 const DB_NAME = 'survey_ques'
-const DB_PASSWORD = ''
+const DB_PASSWORD = process.env.NODE_ENV === 'production' ? process.env.PROD_MYSQL_PASSWORD : ''
 
 module.exports = {
     DB_HOST,
